f4d5b962c9f207e159a4e7f0e8a87a34ea9f2c4a
momento <dkdmfpel@gmail.com>
2022-08-18 17:32:12 +0900

    react management 환경 세팅

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App gray-background">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Let's develop management System!</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
