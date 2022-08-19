efd45e354e63328df01fdc3a617e14e76430a839
momento <dkdmfpel@gmail.com>
2022-08-19 17:13:57 +0900

    커밋테스트입니다

--- src/App.js
+++ src/App.js
@@ -36,7 +36,6 @@ class App extends Component {
         {customers.map((c) => {
           return (
             <Customer
-              key={c.id}
               id={c.id}
               image={c.image}
               name={c.name}
